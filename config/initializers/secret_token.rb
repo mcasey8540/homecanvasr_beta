e65a45d751327c7e7855e5ec5f3fb39aa3c34887
Michael Casey <mcasey8540@gmail.com>
2013-04-11 10:04:01 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomecanvasrBeta::Application.config.secret_token = '778b2401422eb3f6aa8ea491318d26cf112170642dbb930791f84355e58aa21f764744699f03fdd113270af052e75e14790f40b5277a85bfa9891bd4ce67299b'
