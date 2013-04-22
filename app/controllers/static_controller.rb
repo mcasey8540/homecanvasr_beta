42a4fb809c550a3eb3739577365a040db8aadd65
Michael Casey <mcasey8540@gmail.com>
2013-04-22 17:09:44 -0400

    Add Days to homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,12 @@ class StaticController < ApplicationController
   
   def index
   	@subscriber = Subscribe.new
+  	now = Time.now
+  	launch_date = Time.new(2013,5,31)
+
+  	#days
+  	@days = (launch_date - now).to_i / (60*60*24)
+
   end
 
 end
